8fa32809c2dff3e698420de47105006e46ff784c
CaptainAyal <ayalmesher@gmail.com>
2025-03-14 17:48:14 +0200

    working code

--- stockr.rb
+++ stockr.rb
@@ -1,23 +1,30 @@
 #loop through each item in the array. Record the index of the largest differences.
 
+
 def stock_picker (array)
   dis = array.each_with_index.reduce([0, 0, 0]) do |accum, (price, index)|
     i = index
     highest = [0, 0, 0]
+    results = [0, 0, 0]
     while i <= array.length - 2
-      # creates array with price difference, buy index, sell index
       current = [(array[i + 1] - price), index, (i+1)]
       if current[0] > highest[0]
         highest = current
       end
+      results = highest
       i += 1
     end
-    #checks for highest in each index against previous index
-    highest[0] > accum[0] ? highest : accum
+    results[0] > accum[0] ? results : accum
   end
-  #output buy sell only
   p dis.drop(1)
+  
+  
+  # winners = []
 
+  # array.each_with_index.reduce do |accum, (element, index)|
+
+  # end
+  # result
 end
 
 
